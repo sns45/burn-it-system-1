3598a839a1c5a97161ec43b2ff770996d7a240fa
mbefokam <mbefokam@gmail.com>
2017-03-04 20:48:38 -0500

    Joined the login register views to the rest of the application

--- public/deitPlan/controllers/deit.controller.js
+++ public/deitPlan/controllers/deit.controller.js
@@ -5,10 +5,30 @@
         .module("burnIt.deitplan")
         .controller("DeitCtrl", ["CommonSvc", "$location", DeitCtrl]);
 
-    function DeitCtrl(CommonSvc, $location) {
-        var vm = this;
+        function DeitCtrl(CommonSvc, $location) {
+            var vm = this;
+            vm.route = route;
+            function route(param) {
+                switch (param){
+                    case 'save':
+                            $location.path('/planning');
+                            break;
+                    case 'preview':
+                        // save logic
+                        // CommonSvc.set
+                            $location.path('/deit');
+                            break;
+                    case 'edit':
+                            $location.path('/deit');
+                            break;
+                     case 'back':
+                            $location.path('/planning');
+                            break;
+                     case 'dailydeit':
+                            $location.path('/deit/daily');
+                            break;
 
-        //vm.route = route;
-
-    }
+                }
+            }
+        }
 })();
--- public/deitPlan/controllers/deitplan.controller.js
+++ public/deitPlan/controllers/deitplan.controller.js
@@ -6,9 +6,27 @@
         .controller("DeitPlanCtrl", ["CommonSvc", "$location", DeitPlanCtrl]);
 
     function DeitPlanCtrl(CommonSvc, $location) {
-        var vm = this;
+            var vm = this;
+            vm.route = route;
+            function route(param) {
+                switch (param){
+                    case 'save':
+                        $location.path('/deit');
+                        break;
+                   // case 'preview':
+                        // save logic
+                        // CommonSvc.set
+                        /*$location.path('/register');*/
+                       // $location.path('/deit/daily/preview');
+                      //  break;
+                    case 'edit':
+                            $location.path('/deit/daily');
+                            break;
+                     case 'back':
+                            $location.path('/deit');
+                            break;
 
-        //vm.route = route;
-
-    }
+                }
+            }
+        }
 })();
