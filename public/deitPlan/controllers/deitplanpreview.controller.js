1ad00c232af6be5bdc12d7f184d3715e822464c7
mbefokam <mbefokam@gmail.com>
2017-03-04 23:44:38 -0500

    Joined the login register views to the rest of the application

--- public/deitPlan/controllers/deitplanpreview.controller.js
+++ public/deitPlan/controllers/deitplanpreview.controller.js
@@ -5,10 +5,21 @@
         .module("burnIt.deitplan")
         .controller("DeitPlanPreviewCtrl", ["CommonSvc", "$location", DeitPlanPreviewCtrl]);
 
-    function DeitPlanPreviewCtrl(CommonSvc, $location) {
-        var vm = this;
-
-        //vm.route = route;
-
-    }
+   function DeitPlanPreviewCtrl(CommonSvc, $location) {
+            var vm = this;
+            vm.route = route;
+            function route(param) {
+                switch (param){
+                    case 'save':
+                            $location.path('/deit/daily');
+                            break;
+                    case 'delete':
+                        // save logic
+                        // CommonSvc.set
+                            $location.path('/deit/daily/preview');
+                            break;
+                    
+                }
+            }
+        }
 })();
