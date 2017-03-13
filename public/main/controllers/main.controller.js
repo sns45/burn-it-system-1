7162fe035bd9b63b243e17dafdb9433f31f523ac
mbefokam <mbefokam@gmail.com>
2017-03-12 23:34:09 -0400

    I have created a client for yelp api

--- public/main/controllers/main.controller.js
+++ public/main/controllers/main.controller.js
@@ -11,26 +11,55 @@
         vm.genders = ["Male", "Female"];
         vm.person = {};
         vm.reqData = {"weight":{"value":"","unit":"kg"},"height":{"value":" ","unit":"cm"},"sex":"","age":""};
+         var personData ={"first_name":"", "last_name":"","age":"","gender":"","height":"","weight":"","zip_code":""};
         vm.reqBmi = {};
       //  vm.queryFood = {"query":"for breakfast i ate 2 eggs, bacon, and french toast"};
         
         function route(param) {
             switch (param){
                 case 'login':
-                    $location.path('/login');
-                    break;
-                case 'register':
-                    // save logic
-                    // CommonSvc.set
-                    /*$location.path('/register');*/
-                    $location.path('/register');
-                    break;
-                 case 'bmi':
-                 if (!vm.person.age || vm.person.age < 18 || vm.person.age > 100) {
+                    
+                     if (!vm.person.age || vm.person.age < 18 || vm.person.age > 100) {
                      window.alert("You must enter an age between 18 and 100");
                  } else {
-                    // console.log(vm.person);
+                     
                      CommonSvc.setPerson(vm.person);
+                     personData.age = vm.person.age;
+                     personData.gender = vm.person.gender;
+                     personData.height = vm.person.height;
+                     personData.weight = vm.person.weight;
+                     personData.zip_code = vm.person.zipcode;
+                     CommonSvc.setPersonData(personData);
+                     
+                     vm.reqData.weight.value = vm.person.weight;
+                     vm.reqData.height.value = vm.person.height;
+                     vm.reqData.age = vm.person.age;
+                     if (vm.person.gender == "Male"){
+                       vm.reqData.sex = "m"; 
+                     }
+                     else{
+                        vm.reqData.sex = "f"; 
+                       
+                     }
+                      
+                      CommonSvc.setReqData(vm.reqData);  
+                     
+                     $location.path('/login');
+                 }
+                    break;
+                case 'register':
+                     if (!vm.person.age || vm.person.age < 18 || vm.person.age > 100) {
+                     window.alert("You must enter an age between 18 and 100");
+                 } else {
+                     
+                     CommonSvc.setPerson(vm.person);
+                     personData.age = vm.person.age;
+                     personData.gender = vm.person.gender;
+                     personData.height = vm.person.height;
+                     personData.weight = vm.person.weight;
+                     personData.zip_code = vm.person.zipcode;
+                     CommonSvc.setPersonData(personData);
+                     
                      vm.reqData.weight.value = vm.person.weight;
                      vm.reqData.height.value = vm.person.height;
                      vm.reqData.age = vm.person.age;
@@ -42,13 +71,37 @@
                        
                      }
                       CommonSvc.setReqData(vm.reqData);  
-                      /*$http.post('/api/main/bmi',JSON.stringify(reqData) ).then(function(response){
-                      console.log(JSON.stringify(response));
-                      CommonSvc.setReqBmi(response);  */ 
-                      //});
+                     
+                     $location.path('/register');
+                 }
+                    break;
+                 case 'bmi':
+                 /*if (!vm.person.age || vm.person.age < 18 || vm.person.age > 100) {
+                     window.alert("You must enter an age between 18 and 100");
+                 } else {*/
+                     
+                     CommonSvc.setPerson(vm.person);
+                     personData.age = vm.person.age;
+                     personData.gender = vm.person.gender;
+                     personData.height = vm.person.height;
+                     personData.weight = vm.person.weight;
+                     personData.zip_code = vm.person.zipcode;
+                     CommonSvc.setPersonData(personData);
+                     
+                     vm.reqData.weight.value = vm.person.weight;
+                     vm.reqData.height.value = vm.person.height;
+                     vm.reqData.age = vm.person.age;
+                     if (vm.person.gender == "Male"){
+                       vm.reqData.sex = "m"; 
+                     }
+                     else{
+                        vm.reqData.sex = "f"; 
+                       
+                     }
+                      CommonSvc.setReqData(vm.reqData);  
                      
                      $location.path("/planning");
-                 }
+                // }
                     break;
             }
         }
--- routes/main.js
+++ routes/main.js
@@ -1,33 +1,53 @@
  var express = require('express');
  var router = express.Router();
  var request = require("request");
- var empty  = require('is-empty');
-
-
- router.post('/bmi', function(req, res){
-       if(empty(req.body.height.value)  || empty(req.body.weight.value)  || empty(req.body.sex)  ||  empty(req.body.age )){
-           
-            res.send(405, "Please make sure that the height, weight, sex and age values are enter. ");                                            
-       }
-        else{
-            var resObj ={
-        url: "https://bmi.p.mashape.com",
-        method:'POST',
-        json: req.body,
-        headers : {
-            "Content-Type":  "application/json",
-            "X-Mashape-Key": "pKLWGRrBWomshBDBVnH9yriv891rp1XeJNXjsnTwTYcJWwbLdS"
-          }
-       };
-       request(resObj,function(error, response,body){
-       if(error){
-           res.send(response.statusCode);
-       }else{
-           console.log(body);
-           res.send(response.statusCode, body);
-       }   
-    });
-        }     
+ var empty = require('is-empty');
+ router.post('/bmi', function (req, res) {
+     if (empty(req.body.height.value) ||req.body.height.value==""|| empty(req.body.weight.value) || req.body.weight.value==""||empty(req.body.sex) ||req.body.sex==""|| empty(req.body.age)||req.body.age=="") {
+         res.send(405, "Please make sure that the height, weight, sex and age values are enter. ");
+     }
+     else {
+         var resObj = {
+             url: "https://bmi.p.mashape.com"
+             , method: 'POST'
+             , json: req.body
+             , headers: {
+                 "Content-Type": "application/json"
+                 , "X-Mashape-Key": "pKLWGRrBWomshBDBVnH9yriv891rp1XeJNXjsnTwTYcJWwbLdS"
+             }
+         };
+         request(resObj, function (error, response, body) {
+             if (error) {
+                 res.send(response.statusCode);
+             }
+             else {
+                 console.log(body);
+                 res.send(response.statusCode, body);
+             }
+         });
+     }
  });
-module.exports = router;
-
+ router.get('/yelp/:item/location/:zipcode', function (req, res) {
+     if (empty(req.params.zipcode) || req.params.zipcode == "" || req.params.zipcode == null) {
+         res.send(405, "please ensure to specified your zipcode");
+     }
+     else {
+         var resObj = {
+             url: "https://api.yelp.com/v3/businesses/search?term=" + req.params.item + "&location=" + req.params.zipcode
+             , method: 'GET'
+             , json: req.body
+             , headers: {
+                 "Authorization": "Bearer 4rk5uVOv9ciUB_wO_QLMAoFavl87jU2lLl1bcMgkzwPjTly9cpy-GeI93DEA0TWAoTb9lbUEV7g9Yfn1IPvRbO0ouLGFLl3fsqK8m-YKKs99NX2BwS9FFbcsk2PDWHYx"
+             }
+         };
+         request(resObj, function (error, response, body) {
+             if (error) {
+                 res.send(response.statusCode);
+             }
+             else {
+                 res.send(response.statusCode, body);
+             }
+         });
+     }
+ });
+ module.exports = router;
